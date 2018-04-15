e42412a7a139d25d24be1f05f00efbd0c0b9b732
Michael Meyer <mmeyer@michaels-mbp.home>
2018-04-15 21:34:16 +1000

    changed to jquery get method

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,6 @@
 $(document).ready(function(){
 
-   $.ajax({
-            url: "https://boiling-meadow-43049.herokuapp.com/notes",
-            type: 'GET',
-            dataType: 'application/json',
-            success: function(res) {
-               $('#notes').html(res)
-            }
-        });
-
+   $.get("https://boiling-meadow-43049.herokuapp.com/notes", function(data, status) {
+     alert("Data: " + data + "\nStatus: " + status);
+   });
 })
